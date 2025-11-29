47847621a2e12f1b50bb543a3e6a5b97111c025c
nathan-dza <160131368+nathan-dza@users.noreply.github.com>
2025-11-28 23:23:12 -0800

    Add dark mode toggle and improve UI features
    i almost forgot about the dark mode and i started media queries and also made some small edits to the sire

--- script.js
+++ script.js
@@ -1,19 +1,33 @@
+// getting all the elements from html for it to be functional
 const ingredientsTrigger = document.getElementById('ingredients-button');
         const ingredientsPopup = document.getElementById('ingredients-popup');
         const otherTrigger = document.getElementById('other-recipes-button');
         const otherPopup = document.getElementById('other-recipes-popup');
         const closeButtons = document.getElementsByClassName('close');
-
+        const darkModeButton = document.getElementById('dark-mode-button');
+// ingredients popup function
         ingredientsTrigger.addEventListener('click', () => {
             ingredientsPopup.style.display = 'block';
         });
 
+// other recipes popup function
         otherTrigger.addEventListener('click', () => {
             otherPopup.style.display = 'block';
         });
 
-        close.addEventListener('click', () => {
-            ingredientsPopup.style.display = 'none';
-            otherPopup.style.display = 'none';
+// close button function
+        for (let btn of closeButtons) {
+            btn.addEventListener('click', () => {
+                ingredientsPopup.style.display = 'none';
+                otherPopup.style.display = 'none';
+            });
         }
-        );
+//dark mode toggle
+        darkModeButton.addEventListener('click', () => {
+            document.documentElement.classList.toggle('dark-mode');
+            if (document.documentElement.classList.contains('dark-mode')) {
+                darkModeButton.innerText = "Light Mode";
+            } else {
+                darkModeButton.innerText = "Dark Mode";
+            }
+        });
